bc227d66feb8a58130d9aaf2b704cb253bb05c12
leabouffaut <lea.bouffaut@hotmail.fr>
2020-01-17 15:57:39 +0100

    Edits on Offline_save_Zcall + live version

--- Offline_save_Zcall.m
+++ Offline_save_Zcall.m
@@ -1,25 +1,10 @@
 %% Simulate Z-call sigmoid and estimate signal's covariance matrix
 %
-% In this program:
+% In this code:
 % (1) First, the Z-call sigmoid is simulated based on FX. Socheleau 2015 
 % JASA paper.
 % (2) then, the covariance matrix of the signal is evaluated as a first 
 % part of the SMF application
-%
-% ------------------------------------------------------------------------%
-% Author: Lea Bouffaut, PhD
-% Naval Academy Research Institute, Brest, France.
-% Date: 10-30-2019
-% 
-% Full description of the method's theory is described in
-%
-%       L. Bouffaut, R. Dreo, V. Labat, A. Boudraa and G. Barruol 
-%       'Passive stochastic matched filter for antarctic blue whale call
-%       detection,' in J. Acoust. Soc. Am, 144(2) (2018).
-%
-% and any use of this material should refer accordingly. Part of the code
-% was written by Gregory Julien, PhD (2010).
-% ------------------------------------------------------------------------%
 
 
 clearvars
@@ -36,18 +21,16 @@ disp(['Save is: ' Save])
 % Sigmoid parameters
 fs = 100 ; % (Hz) sampling frequency
 
-Tz = 20 ; % (s) duree d'un zcall
+Tz = 20 ; % (s) duration of an ABW call
 tz = 0:1/fs:Tz; % time axis
 N = length(tz); % number of samples
 
 fc = 22.6 ;  % (Hz) Z-call center frequency
 alpha = 1.8; % slope
 
-% Following parameters, 3 values are considered to account for Z-call
-% frequency variations
-L = [-4.5 -4 -3.5]; % Lower assymptote
-U = [3.2 3.6 4];    % Upper asymptote 
-M = [Tz/2 (Tz+0.5)/2 (Tz+1)/2];  % (s) time at half of the slope
+L = -4.5 ;   % (Hz) Lower assymptote
+U = 3.2 ;    % (Hz) Upper asymptote 
+M = Tz/2;    % (s) time at half of the slope
 
 
 % %% Instantaneous frequency
@@ -60,37 +43,39 @@ M = [Tz/2 (Tz+0.5)/2 (Tz+1)/2];  % (s) time at half of the slope
 %     f_whale(i,:) = fc + L(i) + ((U(i)-L(i))./(1+exp(alpha*(tz-M(i)))));
 % end
 
-% Estimation of the time-varying phase 
+%% Estimation of the time-varying phase 
+
 adj = fc - 8.5; % Compared to the Socheleau paper, the sigmoid has to be 
                 % shifted in frequency to match the Z-call signal
-L = L-adj;
-U = U-adj;
+L = L-adj; % Lower asymptote (Hz)
+U = U-adj; % Upper asymptote (Hz)
 
-phase_whale = zeros(1,N);
-s_whale = zeros(1,N);
-i = 1;
-for n = 0:N-1
-    phase_whale(n+1) = 2*pi*(L(i)*n/fs + ((U(i)-L(i))/alpha)*log((1+exp(-alpha*M(i)))/(1+exp(alpha*(n/fs-M(i)))))); % Expression de laphase
-end
-phase_whale = phase_whale(end:-1:1);
-s_whale = s_whale + exp(1j*phase_whale); % temporal signal creation
+% Phase calculation
+n = 0:N-1; % Sample axis
+phase_whale = 2*pi*(L*n/fs + ((U-L)/alpha) * ...
+    log((1+exp(-alpha*M))./(1+exp(alpha*(n/fs-M)))));
+phase_whale = phase_whale(end:-1:1);    % Reverse in time to have it from high to low frequency
 
+% Temporal signal
+s_whale = exp(1j*phase_whale); 
+s_whale = real(s_whale/max(abs(s_whale))); 
 
-% Amplitude
-s_whale = s_whale/max(abs(s_whale)); 
+% Amplitude variation through time
 amplitude = [ones(1,round(N/2)-1) ones(1,round(N/2))*0.95];
-p0 = 1e-6;
-s_whale = amplitude.*s_whale*p0;
-s_whale = real(s_whale/max(abs(real(s_whale))));
+s_whale = amplitude.*s_whale;
+s_whale = s_whale/max(abs(s_whale));
 
 
-%% Calcul de la martice de covariance du signal
-[covs,vecs] = SMF_sig_preprocess(real(s_whale)) ;
+%% Covariance matrix estimation
+[covs,vecs] = SMF_sig_preprocess(s_whale) ;
 
-if strcmp(Save,'On') == 1
-save('Offline_saved/s_whale.mat', 's_whale','covs','vecs')
-disp('Matrices saved in: Offline_saved/s_whale.mat')
-else disp('Matrices not saved')
+switch Save
+    case 'On'
+        save('Offline_saved/s_whale.mat', 's_whale','covs','vecs')
+        disp('Matrices saved in: Offline_saved/s_whale.mat')
+    case 'Off' 
+        disp('Matrices not saved')
+    otherwise warning('Uknown Save command - matrices not saved')
 end
 
 
